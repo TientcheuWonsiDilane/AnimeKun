dbdaa7efd506774ea29b72fe92685135add06e7c
TientcheuWonsiDilane <dilanetientcheu67@gmail.com>
2026-04-09 19:05:41 +0100

    cors debug3

--- server/src/index.js
+++ server/src/index.js
@@ -12,12 +12,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://animekun.up.railway.app'],
+  origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  
 }));
-app.options('*', cors());
 app.use(express.json());
 
 connectDB();
